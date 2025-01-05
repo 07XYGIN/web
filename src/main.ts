23d3f6f6c04614c356780181fd1c15a90e607fd7
xygin <xygin0708@163.com>
2025-01-05 11:50:59 +0800

    feat eleuiplus--icon全部导入

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/router'
-import store from './store'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import './index.css'
-import './tallwind.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/router';
+import store from './store';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import './index.css';
+import './tallwind.css';
 
-createApp(App).use(ElementPlus).use(router).use(store).mount('#app')
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+	app.component(key, component);
+}
+app.use(ElementPlus);
+app.use(router);
+app.use(store);
+app.mount('#app');
